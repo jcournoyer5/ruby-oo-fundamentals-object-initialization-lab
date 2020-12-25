8bdf1a54de851511727deff56283b79cd6184b8c
Jacqueline Cournoyer <jcournoyer5@gmail.com>
2020-12-25 04:35:13 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,15 @@
 class Dog
-  def initialize(breed)
-    @breed = breed
+
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
 
-end 
+  def name
+    @this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
